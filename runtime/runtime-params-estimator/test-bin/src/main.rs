31d1db7064e6a60e0f174024f536e3f44e6d0d04
Longarithm <the.aleksandr.logunov@gmail.com>
2021-09-07 14:44:18 +0300

    test

---
+++ runtime/runtime-params-estimator/test-bin/src/main.rs
@@ -0,0 +1,15 @@
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
+    let build_test_contract = "echo 123";
+    let output = std::process::Command::new(build_test_contract)
+        .output()
+        .with_context(|| format!("failed to run `{}`", build_test_contract))?;
+    if !output.status.success() {
+        anyhow::bail!("failed to run `{}`", build_test_contract);
+    }
+    let stdout = String::from_utf8(output.stdout)
+        .with_context(|| format!("failed to run `{}`", build_test_contract))?;
+    println!("{}", stdout.trim().to_string());
+    Ok(())
+}
