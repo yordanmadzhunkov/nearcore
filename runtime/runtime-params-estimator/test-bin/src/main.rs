3455688cc48e738db8e2fcda29e56d4b6cb4d69d
Longarithm <the.aleksandr.logunov@gmail.com>
2021-09-07 14:51:48 +0300

    test

--- runtime/runtime-params-estimator/test-bin/src/main.rs
+++ runtime/runtime-params-estimator/test-bin/src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
-    let build_test_contract = "echo 123";
-    let output = std::process::Command::new(build_test_contract).output().unwrap();
+    let build_test_contract = "echo";
+    let output = std::process::Command::new(build_test_contract).args(["123"]).output().unwrap();
     let out = String::from_utf8(output.stdout.clone())?;
     let err = String::from_utf8(output.stderr.clone())?;
     println!("{}", &out);
